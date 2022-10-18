b51d36eb7d506e8a371d409dffcc84a6b32c9ecf
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 10:20:09 -0500

    3

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 addEventListener(`DOMContentLoaded`,()=>{
-    // #2Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
+    // #3Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o si son iguales
     // #entrada de datos
-    let num1=Number(prompt(`ingrese el pirmer numero`))
-    let num2=Number(prompt(`ingrese el segundo numero`))
-    let num3=Number(prompt(`ingrese el tercero numero`))
+    let num1=Number(prompt(`ingrese el pirmer numero:`))
+    let num2=Number(prompt(`ingrese el segundo numero:`))
+    let num3=Number(prompt(`ingrese el tercero numero:`))
     // #proceso
+    // #esta parte es de mayor
     if (num1>num2 && num1>num3){
         alert(`El numero 1 es mayor`)
-    }
-
-    if (num2>num3 && num2>num1){
+    }else if (num2>num3 && num2>num1){
         alert(`El numero 2 es mayor`)
     }else{
         alert(`el numero 3 es mayor`)
-    }
+
+    }if (num1<=num2 && num1<=num3){
+        alert(`El numero 1 es menor`)
+    }else if (num2<=num3 && num2<=num1){
+        alert(`El numero 2 es menor`)
+    }else{alert(`el numero 3 es menor`)
 
 
+    } if(num1==num2){
+         alert(`El numero 1 y 2 son iguales `)
+    }else if (num2==num3){
+        alert(`El numero 2 y 3 son iguales`)
+    }else if (num3==num1){
+        alert(`el numero 1 y 3 son iguales`) 
+    }  
 })
