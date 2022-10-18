114f877f480238ca1824189edf75c7e8f1e92e88
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 10:19:39 -0500

    2

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 addEventListener(`DOMContentLoaded`,()=>{
-    // #1DEFINIR SI UN NUMERO ES POSITIVO Y MENOR QUE 100.
+    // #2Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
     // #entrada de datos
-    let N=Number(prompt(`ingrese el numero positivo del 1 al 100`))
-    if(N<100 && N>0){
-        console.log(`el numero es :${N} y esta en el rango estipulado`)
-        console.log(`el numero es :${N} y esta en el rango estipulado`)
-    }else{
-        console.log(`el numero tiene que es tar entre el rango estipulado`)
-        console.log(`el numero tiene que es tar entre el rango estipulado`)
+    let num1=Number(prompt(`ingrese el pirmer numero`))
+    let num2=Number(prompt(`ingrese el segundo numero`))
+    let num3=Number(prompt(`ingrese el tercero numero`))
+    // #proceso
+    if (num1>num2 && num1>num3){
+        alert(`El numero 1 es mayor`)
     }
+
+    if (num2>num3 && num2>num1){
+        alert(`El numero 2 es mayor`)
+    }else{
+        alert(`el numero 3 es mayor`)
+    }
+
 
 })
