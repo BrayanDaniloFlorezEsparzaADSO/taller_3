ba6c3059b461005fd10ddede26d54b141a05d7b5
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 10:24:52 -0500

    7

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 addEventListener(`DOMContentLoaded`,()=>{
-    //     #6Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
+    // #Calcular todos los pagos hechos de un restaurante y que si el consumo ingresado excede los$130.000 el descuento será del 15%, de lo contrario no hay descuento.
     // #ENTRADA DE DATOS
-    let temperatura=Number(prompt(`coloque la temperatura`))
-    let precion=Number(prompt(`coloque la precion`))
+    let pago1=Number(prompt("dijite el valor total a pagar"))
+    let desc=0.15
     // #proceso
-    if (precion>200){
-         alert(`ALERTA EN LA PRECION:,${precion}`)
+    if (pago1 >=130000){
+        let res1=pago1*desc
+        let res2=pago1-res1
+        alert(`su valor total con el 15% de descuento es de: ${res2}`)
     }else{
-        alert(`NORMAL EN LA PRECION:,${precion}`)
-    
+        alert(`no tiene el 15% de descuento ya que no cumplio con la cuota del restaurante`)
+        alert(`el valor a pagar es de: ${pago1}`)
     }
-    if (temperatura>100){
-        alert(`ALERTA EN LA TEMPERATURA:${temperatura}`)
-    }else{
-        alert(`NORMALEN LA TEMPERATURA:${temperatura}`)    
-    }
-    
 })
