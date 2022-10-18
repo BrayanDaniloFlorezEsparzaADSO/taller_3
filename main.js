f1edd39d0294a71f310126579d8d41e7910544a9
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 10:20:48 -0500

    4

--- main.js
+++ main.js
@@ -1,30 +1,17 @@
 addEventListener(`DOMContentLoaded`,()=>{
-    // #3Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o si son iguales
-    // #entrada de datos
-    let num1=Number(prompt(`ingrese el pirmer numero:`))
-    let num2=Number(prompt(`ingrese el segundo numero:`))
-    let num3=Number(prompt(`ingrese el tercero numero:`))
-    // #proceso
-    // #esta parte es de mayor
-    if (num1>num2 && num1>num3){
-        alert(`El numero 1 es mayor`)
-    }else if (num2>num3 && num2>num1){
-        alert(`El numero 2 es mayor`)
-    }else{
-        alert(`el numero 3 es mayor`)
-
-    }if (num1<=num2 && num1<=num3){
-        alert(`El numero 1 es menor`)
-    }else if (num2<=num3 && num2<=num1){
-        alert(`El numero 2 es menor`)
-    }else{alert(`el numero 3 es menor`)
-
-
-    } if(num1==num2){
-         alert(`El numero 1 y 2 son iguales `)
-    }else if (num2==num3){
-        alert(`El numero 2 y 3 son iguales`)
-    }else if (num3==num1){
-        alert(`el numero 1 y 3 son iguales`) 
-    }  
+    // #4Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
+    // #ENTRADA DE DATOS
+    let figura=prompt(`elija una figura triangulo(t) o circulo(c):`)
+    // #PROCESO
+    if (figura== `t`){
+        let base=Number(prompt(`coloque la base del  triangulo:`))
+      let altura=Number(prompt(`coloque la altura del triangulo:`))
+      let res=(base*altura)/2
+      alert(`el area del triangulo es de:${res}`)
+    }else if (figura=="c"){
+        let pi=3.14
+      let radio=Number(prompt(`coloque el radio:`))
+      let res=(pi*radio)**2
+      alert(`el area del circulo es de:${res}`)
+    }
 })
