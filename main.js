c7ad97a799f5774e0324ae9b44af750032259165
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-18 10:18:52 -0500

    1

---
+++ main.js
@@ -0,0 +1,13 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    // #1DEFINIR SI UN NUMERO ES POSITIVO Y MENOR QUE 100.
+    // #entrada de datos
+    let N=Number(prompt(`ingrese el numero positivo del 1 al 100`))
+    if(N<100 && N>0){
+        console.log(`el numero es :${N} y esta en el rango estipulado`)
+        console.log(`el numero es :${N} y esta en el rango estipulado`)
+    }else{
+        console.log(`el numero tiene que es tar entre el rango estipulado`)
+        console.log(`el numero tiene que es tar entre el rango estipulado`)
+    }
+
+})
